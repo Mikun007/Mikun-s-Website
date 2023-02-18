72619de8940b4285925ec06f1a3cef2e43eb8764
mikun <mikundas@outlook.com>
2023-02-18 10:35:37 +0530

    full setup completed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+    return (
+        <div>
+            <div id="gird">
+                <header>Header</header>
+                <main>Main1</main>
+                <main>Main2</main>
+                <footer>footer</footer>
+            </div>
+            <nav>Nav</nav>
+        </div>
+    );
+};
+
+export default App;
