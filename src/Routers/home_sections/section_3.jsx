016224ab662976f9cc38c7086bb0541df81987fd
mikun <mikundas@outlook.com>
2023-02-21 11:57:20 +0530

    Finished Section_3 of Home Page

--- src/Routers/home_sections/section_3.jsx
+++ src/Routers/home_sections/section_3.jsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+
 function Section_3() {
     return(
         <section id="third_section_home">
-            section3
+            <Link>
+                <h3>Experience..</h3>
+                <p>Making a Website from scrach by your-self is really pain 🙂 but at the same time it is really 
+                    Fascinating and Exciting. 😍🤩</p>
+                <i class="fa-solid fa-angles-right fa-3x"></i>
+            </Link>
+            <a href="https://www.skillsyouneed.com/rhubarb/benefits-of-typing.html" target="_blank">
+                <h3>DO YOU KNOW</h3>
+                <p>Typing Speed May not seem intimidating but it really will help you in many aspect of life.... 🧐</p>
+                <i class="fa-solid fa-angles-right fa-2x fa-3x"></i>
+            </a>
+            <a href="">
+                <p>Why Cardio Exercise is Important but with Limitation...🤨</p>
+                <i class="fa-solid fa-angles-right fa-3x"></i>
+            </a>
         </section>
     );
 };
